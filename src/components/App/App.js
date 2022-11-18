16dea37323558e60326604c5f20c1faa8b975d44
VincentVanN <vinc.vannieu@gmail.com>
2022-11-18 12:13:42 +0100

    init

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+// == Import
+import './app.scss';
+
+// == Composant
+function App() {
+  return (
+    <div className="app">
+      {/* <Game rows={20} colomns={10} /> */}
+    </div>
+  );
+}
+
+// == Export
+export default App;
